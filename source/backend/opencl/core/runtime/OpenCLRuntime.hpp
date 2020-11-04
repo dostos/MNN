8ef04b7aa0b68df578906c3d3614b3e44d6902ff
Jingyu Lee <dostos10@gmail.com>
2020-11-04 08:10:46 +0000

    Opencl - lws cache load / save

--- source/backend/opencl/core/runtime/OpenCLRuntime.hpp
+++ source/backend/opencl/core/runtime/OpenCLRuntime.hpp
@@ -108,6 +108,8 @@ private:
     double mStartNanos;
     double mStopNanos;
 
+    static const char *sLwsCache;
+
     std::map<std::pair<std::string, std::vector<uint32_t>>, std::vector<uint32_t>> mTunedLws;
 
 };
